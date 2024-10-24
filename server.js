e1c77877dc2de9432fc568fb2f96f753963a80d6
wdiasmaciel <wdiasmaciel@gmail.com>
2024-10-24 21:18:30 +0000

    Atualizei server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ server.use(cors()); // Habilita CORS (Cross-Origin Resource Sharing).
 server.use(middlewares);
 server.use(router);
 
+// Adiciona a configuração de middleware para CORS (Cross-Origin Resource Sharing).
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Permite qualquer origem.
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
 server.listen(3333, () => {
   console.log('JSON-Server is running!');
 });
